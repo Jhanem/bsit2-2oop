bb6452b4548bdf56fb4303b27efddf354dda6162
Jhan Mandahinog <jlmandahinog65115@liceo.edu.ph>
2025-07-31 11:50:22 +0800

    Activity lab 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main{
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Input your Student Id, FirstName, LastName, Course, Section.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Your Id: ");
+        String studentId = scanner.nextLine();
+
+        System.out.println("First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Course: ");
+        String course = scanner.nextLine();
+
+        System.out.println("Section: ");
+        String section = scanner.nextLine();
+
+        System.out.println("STUDENT INFORMATION");
+        System.out.println("student id: " + studentId );
+        System.out.println("student name: " + firstName + lastName );
+        System.out.println("Course: " + course);
+        System.out.println("Section: " + section);
+
+
+        System.out.println("Midterm Exam Score: ");
+        int midtermExamScore = scanner.nextInt();
+        System.out.println("Final Exam Score : ");
+        int finalExamScore = scanner.nextInt();
+        System.out.println("Project Score: ");
+        int projectScore = scanner.nextInt();
+        System.out.println("Attendance Percentage: ");
+        int attendancePercentage = scanner.nextInt();
+
+        int allOverScore = midtermExamScore + finalExamScore + projectScore + attendancePercentage;
+        int averageScore = allOverScore / 400;
+
+        System.out.println("STUDENT SCORE");
+        System.out.println("Midterm Exam Score: " + midtermExamScore);
+        System.out.println("Final Exam Score : " + finalExamScore);
+        System.out.println("Project Score: " + projectScore);
+        System.out.println("Attendance Score: " + attendancePercentage);
+
+        System.out.println(" ");
+        System.out.println("Average Score: " + averageScore);
+        if(averageScore < 75){
+            System.out.println("Remarks: " + "Failed");
+        }else{
+            System.out.println("Remarks: " + "Passed");
+        }
+
+
+
     }
 }
